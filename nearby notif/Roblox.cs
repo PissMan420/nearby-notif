4abedf1ccd53b4eb25f08a0af4e803e0bfbdf27c
Vini Dalvino <vinidalvino@github.com>
2020-08-01 18:25:14 -0400

    MAde the roblox class more oop

---
+++ nearby notif/Roblox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+
+
+namespace nearby_notif
+{
+    class RobloxStat
+    {
+        public int Value { get; }
+        public int RecentAvetagePrice { get;  }
+        RobloxStat(int Value, int RAP) {
+            this.Value = Value;
+            this.RecentAvetagePrice = RAP;
+        }
+    }
+
+    class RobloxPlayer
+    {
+        public Int64 UserId { get; }
+        public string Username { get; }
+
+        RobloxPlayer(int userId, string username) {
+            this.UserId = userId;
+            this.Username = username;
+        }
+    }
+}
